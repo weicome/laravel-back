c6648503865ac55159d4c10b68e4f047778c635f
weicome <407888190@qq.com>
2023-09-04 19:14:23 +0800

    init

---
+++ database/migrations/2023_09_04_100040_create_admin_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admin_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username',20)->default('')->comment('昵称');
+            $table->string('account',64)->default('')->comment('账号');
+            $table->string('password',64)->default('')->comment('密码');
+            $table->tinyInteger('status')->default(1)->comment('状态');
+            $table->ipAddress()->comment('登录ip地址');
+            $table->timestamps();
+            $table->comment('管理员账号表');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admin_users');
+    }
+};
